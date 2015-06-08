d50761b619568bdda728a7c1e5a3d319d7abd35f
Redwards77 <rcpe77@gmail.com>
2015-06-08 17:16:45 -0400

    Added a new AA table
    Attempted to help Zach with his wavelets

--- wavelet.R
+++ wavelet.R
@@ -1,7 +1,8 @@
 Haarwavelet = function(firstRow, endRow){
-  n=length(Row_1)
   
-  fRow_new = function(Row_prev, Row_next, Row_no){
+  k = endRow
+  
+  fRow_new = function(Row_prev,Row_next,Row_no){
     m = Row_no
     Row_next = c()
     n = length(Row_prev)
@@ -34,5 +35,24 @@ Haarwavelet = function(firstRow, endRow){
     }
     return(Row_next)
   }
+  i = 1:8
+  j=c(0:3)
+  n= mapply(function(j){i*2^j},j)
+  
+  
+  Averages = matrix(c(-12, 0, 12, 16, 12, 0, -12, -16),1,8)
+  Row <- c()
+  fAvg = function(Averages){
+    (Averages[nrow(Averages),n[i-1,1]]+Averages[nrow(Averages),n[(i),1]])/2
   }
+  Rows <- function(Averages){for(i in 1:8){
+    if(i == 1){
+      Row[i] <- (Averages[nrow(Averages),n[i,1]])/2
+    }
+    else{
+      Row[i] <- (Averages[nrow(Averages),n[i-1,1]]+Averages[nrow(Averages),n[(i),1]])/2
+  }
+  }
+    rbind(Averages,Row)}
+}
 
